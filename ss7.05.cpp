ad6a908796b26ebcbd70e6484e6023a10414c197
hoang1811-coder <vhoangssz1@gmail.com>
2024-11-28 04:37:03 +0700

    uopload

---
+++ ss7.05.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int arr[5] = {76, 57, 24, 91, 54};
+    
+    int max = arr[0];  
+    int min = arr[0]; 
+    for (int i = 1; i < 5; i++) {
+        if (arr[i] > max) {
+            max = arr[i];  
+        }
+        if (arr[i] < min) {
+            min = arr[i];  
+        }
+    }
+
+    printf("Phan tu lon nhat trong mang: %d\n", max);
+    printf("Phan tu nho nhat trong mang: %d\n", min);
+
+    return 0;
+}
